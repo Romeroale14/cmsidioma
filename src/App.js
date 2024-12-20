46208a3e62285f235bcd49d1b1f7e6737c75ed0d
Romeroale14 <alexisramrom@outlook.com>
2024-12-20 14:39:07 -0600

    20diciembre2024

--- src/App.js
+++ src/App.js
@@ -1,49 +1,36 @@
-import { useEffect, useState, useCallback } from 'react'; // Importación de hooks de React: useEffect, useState, y useCallback
-import './App.css'; // Importa el archivo CSS de la aplicación
-import useContentful from './useContentful'; // Importa el hook personalizado para interactuar con Contentful
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa el JavaScript de Bootstrap
-import ReactPlayer from 'react-player'; // Importa el componente ReactPlayer para mostrar videos
-import { ClipLoader } from 'react-spinners'; // Importa el spinner de react-spinners
-import espanol from './assets/espana.png'; // Importa una imagen (España) para el navbar
+import { useEffect, useState } from 'react';
+import './App.css';
+import useContentful from './useContentful';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { ClipLoader } from 'react-spinners';
+import espanol from './assets/espana.png';
+
 
 function App() {
-  const [authors, setAuthors] = useState([]); // Estado para almacenar los autores obtenidos
-  const { getAuthors } = useContentful(); // Obtiene la función getAuthors desde el hook useContentful
-  const [menuOpen, setMenuOpen] = useState(false); // Estado para controlar si el menú está abierto
-  const [loading, setLoading] = useState(true); // Estado para controlar el estado de carga
-  const [error, setError] = useState(null); // Estado para manejar los errores
-
-  // Usa useCallback para memorizar la función getAuthors y evitar que se redefine en cada render
-  const memoizedGetAuthors = useCallback(getAuthors, [getAuthors]);
+  const [authors, setAuthors] = useState([]);
+  const { getAuthors } = useContentful();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => { // Función para obtener los datos de los autores
+    const fetchData = async () => {
       try {
-        setLoading(true); // Activa el estado de carga
-        const response = await memoizedGetAuthors(); // Llama a la función memoizada
-        if (response) { // Si la respuesta es válida, actualiza el estado de autores
-          setAuthors(response || []);
-        } else { // Si no hay respuesta, establece una lista vacía
-          setAuthors([]);
-        }
-      } catch (err) { // Si hay un error en la solicitud, maneja el error
+        setLoading(true);
+        const response = await getAuthors();
+        setAuthors(response || []);
+      } catch (err) {
         setError('Error al cargar los autores');
       } finally {
-        setLoading(false); // Desactiva el estado de carga al finalizar
+        setLoading(false);
       }
     };
 
-    if (authors.length === 0) { // Solo ejecuta fetchData si no hay autores cargados
-      fetchData(); // Llama a fetchData
+    if (authors.length === 0) {
+      fetchData();
     }
-  }, [memoizedGetAuthors, authors.length]); // Dependencias: memoizedGetAuthors y la longitud de authors
+  }, [getAuthors, authors.length]);
 
-  const toggleMenu = () => { // Función para alternar el estado del menú
-    setMenuOpen(!menuOpen);
-  };
-
-  // Si los datos aún están cargando, muestra el spinner
   if (loading) {
     return (
       <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
@@ -52,7 +39,6 @@ function App() {
     );
   }
 
-  // Si hay un error, muestra el mensaje de error
   if (error) {
     return (
       <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
@@ -61,146 +47,42 @@ function App() {
     );
   }
 
+
   return (
-    <div
-      style={{
-        backgroundImage: authors.length > 0 ? `url(${authors[0].bodyy})` : 'none', // Establece la imagen de fondo si hay autores
-        backgroundSize: 'cover', // Asegura que la imagen de fondo cubra toda el área
-        backgroundPosition: 'center', // Centra la imagen de fondo
-        minHeight: '100vh', // Hace que el fondo cubra toda la altura de la pantalla
-      }}
-    >
-      <header className="header">
-        <div>
-          {authors.length > 0 ? (
-            authors.map((author, index) => ( // Si hay autores, mapea y muestra el logo
-              <a id="mi-enlace2" href="/" className="logo2" key={index}>
-                <img src={author.logoPa} alt="logo" />
-              </a>
-            ))
-          ) : null}
-        </div>
+    <div>
 
-        {/* Navbar */}
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={toggleMenu}
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded={menuOpen ? 'true' : 'false'}
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
-            {authors.length > 0 ? (
-              authors.map((author, index) => ( // Si hay autores, mapea y muestra los elementos del menú
-                <ul className="navbar-nav" key={index}>
-                  <li className="nav-item">
-                    <a id="mi-enlace" href="/">
-                      <img src={author.ingles} alt="Inglés" />
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a id="mi-enlace" href="/">
-                      <img src={espanol} alt="Español" />
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a id="mi-enlace" href="/">
-                      <img src={author.frances} alt="Francés" />
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a id="mi-enlace" href="/">
-                      <img src={author.italiano} alt="Italiano" />
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a id="mi-enlace" href="/">
-                      <img src={author.china} alt="Chino" />
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a id="mi-enlace" href="/">
-                      <img src={author.aleman} alt="Alemán" />
-                    </a>
-                  </li>
-                </ul>
-              ))
-            ) : null}
-          </div>
-        </nav>
-      </header>
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <div>
-        {authors.length > 0 ? (
-          authors.map((author, index) => ( // Si hay autores, muestra el ReactPlayer con el video
-            <ReactPlayer
-              key={index}
-              url={author.videoRep}
-              width="820"
-              height="340"
-              controls={false}
-              playing
-              muted // Silencia el video al inicio
-              loop // Hace que el video se repita en bucle
-              id="myVideoContainer"
-            />
-          ))
-        ) : null}
+      <div className='navegador'>
+        {authors.length > 0 &&
+          authors.map((author, index) => (
+            <ul class="nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="#"><img src={author.aleman} className='navimg'  alt="Alemán" /></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src={author.china} className='navimg'   alt="Chino" /></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src={author.italiano} className='navimg'   alt="Italiano" /></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#"><img src={author.frances} className='navimg'   alt="Francés" /></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#"><img src={espanol} className='navimg'   alt="Español" /></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#"><img src={author.ingles} className='navimg'   alt="Ingles" /></a>
+              </li>
+            </ul>
+          ))}
       </div>
 
-      {/* CARRUSEL */}
-      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-        <div>
-          {authors.length > 0 ? (
-            authors.map((author, index) => ( // Si hay autores, muestra el carrusel
-              <div className="carousel-inner" key={index}>
-                <div className="carousel-item active">
-                  <img src={author.avatar} className="d-block w-100" alt="First Slide" />
-                </div>
-                <div className="carousel-item">
-                  <img src={author.carrusel2} className="d-block w-100" alt="Second Slide" />
-                </div>
-                <div className="carousel-item">
-                  <img src={author.carrusel1} className="d-block w-100" alt="Third Slide" />
-                </div>
-              </div>
-            ))
-          ) : null}
-        </div>
 
-        {/* BOTONES CARRUSEL */}
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
     </div>
+
+
+
   );
 }
 
-export default App; // Exporta el componente App para usarlo en otras partes de la aplicación
+export default App;
---
+++ src/xd.js
@@ -0,0 +1,242 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import useContentful from './useContentful';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { ClipLoader } from 'react-spinners';
+import espanol from './assets/espana.png';
+import read from './assets/read.png';
+import speak from './assets/speak.png';
+import under from './assets/understand.png';
+import write from './assets/write.png';
+
+import ReactPlayer from 'react-player';
+
+function App() {
+  const [authors, setAuthors] = useState([]);
+  const { getAuthors } = useContentful();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await getAuthors();
+        setAuthors(response || []);
+      } catch (err) {
+        setError('Error al cargar los autores');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (authors.length === 0) {
+      fetchData();
+    }
+  }, [getAuthors, authors.length]);
+
+  useEffect(() => {
+    // Aplicar el fondo dinámico
+    if (authors.length > 0) {
+      document.body.style.backgroundImage = `url(${authors[0].bodyy})`;
+      document.body.style.backgroundSize = 'cover';
+      document.body.style.backgroundPosition = 'center';
+      document.body.style.backgroundAttachment = 'fixed';
+      document.body.style.backgroundRepeat = 'no-repeat';
+    }
+    return () => {
+      // Limpiar el fondo al desmontar el componente
+      document.body.style.backgroundImage = '';
+    };
+  }, [authors]);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <ClipLoader color="#007bff" loading={loading} size={75} />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <span>{error}</span>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <header className="header">
+        <div>
+          {authors.length > 0 &&
+            authors.map((author, index) => (
+              <a id="mi-enlace2" href="/" className="logo2" key={index}>
+                <img src={author.logoPa} alt="logo" />
+              </a>
+            ))}
+        </div>
+
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={toggleMenu}
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded={menuOpen ? 'true' : 'false'}
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
+            {authors.length > 0 &&
+              authors.map((author, index) => (
+                <ul className="navbar-nav" key={index}>
+                  <li className="nav-item">
+                    <a id="mi-enlace" href="/">
+                      <img src={author.ingles} alt="Inglés" />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a id="mi-enlace" href="/">
+                      <img src={espanol} alt="Español" />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a id="mi-enlace" href="/">
+                      <img src={author.frances} alt="Francés" />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a id="mi-enlace" href="/">
+                      <img src={author.italiano} alt="Italiano" />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a id="mi-enlace" href="/">
+                      <img src={author.china} alt="Chino" />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a id="mi-enlace" href="/">
+                      <img src={author.aleman} alt="Alemán" />
+                    </a>
+                  </li>
+                </ul>
+              ))}
+          </div>
+        </nav>
+      </header>
+     
+
+      <div className="intro">
+        <h1>About</h1>
+        <h5>
+          At WorldWide Certification, we are dedicated to providing language certification programs that meet the highest
+          international standards.
+        </h5>
+
+        <h5>
+          Our goal is to offer our users an official validation of their language skills facilitating their access to new
+          academic, professional, and cultural opportunities in a globalized context.
+        </h5>
+
+        <div className='intro'>
+          <h1>What do you learn?</h1>
+          <ul>
+            <li className="segundo">
+              <img src={read} style={{ width: '80px', height: '70px' }} />
+            </li>
+            <li className="segundo">
+              <img src={under} style={{ width: '80px', height: '70px' }} />
+            </li>
+            <li className="segundo">
+              <img src={speak} style={{ width: '80px', height: '70px' }} />
+            </li>
+            <li className="segundo">
+              <img src={write} style={{ width: '80px', height: '70px' }} />
+            </li>
+          </ul>
+
+        </div>
+
+
+
+      </div>
+
+      <div>
+        {authors.length > 0 ? (
+          authors.map((author, index) => ( // Si hay autores, muestra el ReactPlayer con el video
+            <ReactPlayer
+              key={index}
+              url={author.videoRep}
+              width="820"
+              height="340"
+              controls={false}
+              playing
+              muted // Silencia el video al inicio
+              loop // Hace que el video se repita en bucle
+              id="myVideoContainer"
+            />
+          ))
+        ) : null}
+      </div>
+
+
+
+
+
+      {/* CARRUSEL */}
+      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div>
+          {authors.length > 0 ? (
+            authors.map((author, index) => (
+              <div className="carousel-inner" key={index}>
+                <div className="carousel-item active">
+                  <img src={author.avatar} className="d-block w-100" alt="First Slide" />
+                </div>
+                <div className="carousel-item">
+                  <img src={author.carrusel2} className="d-block w-100" alt="Second Slide" />
+                </div>
+                <div className="carousel-item">
+                  <img src={author.carrusel1} className="d-block w-100" alt="Third Slide" />
+                </div>
+              </div>
+            ))
+          ) : null}
+        </div>
+
+        {/* BOTONES CARRUSEL */}
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+          data-bs-ride="carousel"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
